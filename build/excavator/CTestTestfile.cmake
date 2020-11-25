98af387780a2b15d006bd58e6c669696167194bf
欧阳俊源AutoB17 <706545330@qq.com>
2020-11-25 13:25:38 +0800

    add camera

--- build/excavator/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ou/workspace/ugv_ws/src/excavator
-# Build directory: /home/ou/workspace/ugv_ws/build/excavator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
